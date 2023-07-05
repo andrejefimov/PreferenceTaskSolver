c8d2b610b6d06dbdfb52e1543ddac8d31750cf49
Andrej Efimov <andrej.efimov@gmail.com>
2023-07-05 17:52:58 +0300

    another real task

--- tests/pref/tasks/RealTasks.java
+++ tests/pref/tasks/RealTasks.java
@@ -17,6 +17,7 @@ import static pref.Value.Queen;
 public class RealTasks {
     @Test
     public void task1() {
+        // https://review-pref.ru/literatura/54/149/ puzzle 5
         Hand firstHand = new Hand(Arrays.asList(
                 card(Spades, Ace),
                 card(Spades, King),
@@ -55,5 +56,48 @@ public class RealTasks {
         NormalGameState normalGameState = new NormalGameState(hands, new RoundState(), 2);
 
         Assertions.assertEquals(5, normalGameState.numOfTricksWithTrump(Clubs, 0));
+    }
+
+    @Test
+    public void task2() {
+        // https://review-pref.ru/literatura/54/149/ puzzle 2
+        Hand firstHand = new Hand(Arrays.asList(
+                card(Spades, Ace),
+                card(Spades, King),
+                card(Spades, Jack),
+                card(Spades, Ten),
+                card(Spades, Nine),
+                card(Diamonds, King),
+                card(Diamonds, Eight),
+                card(Hearts, Jack),
+                card(Hearts, Nine),
+                card(Hearts, Seven)));
+        Hand secondHand = new Hand(Arrays.asList(
+                card(Spades, Queen),
+                card(Clubs, Ace),
+                card(Clubs, Jack),
+                card(Clubs, Ten),
+                card(Diamonds, Jack),
+                card(Diamonds, Ten),
+                card(Diamonds, Nine),
+                card(Hearts, King),
+                card(Hearts, Ten),
+                card(Hearts, Eight)));
+        Hand thirdHand = new Hand(Arrays.asList(
+                card(Clubs, King),
+                card(Clubs, Queen),
+                card(Clubs, Nine),
+                card(Clubs, Eight),
+                card(Clubs, Seven),
+                card(Diamonds, Ace),
+                card(Diamonds, Queen),
+                card(Diamonds, Seven),
+                card(Hearts, Ace),
+                card(Hearts, Queen)));
+
+        List<Hand> hands = Arrays.asList(firstHand, secondHand, thirdHand);
+        NormalGameState normalGameState = new NormalGameState(hands, new RoundState(), 2);
+
+        Assertions.assertEquals(7, normalGameState.numOfTricksWithTrump(Clubs, 2));
     }
 }
